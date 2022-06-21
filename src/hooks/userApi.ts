fae06d22ce8914c374929fb1e1e68b3f42c6d872
Rodrigo José <rodrigo.angelis@grupoeureka.com.br>
2022-06-21 18:40:24 -0300

    api "fantasma" apenas para teste

--- src/hooks/userApi.ts
+++ src/hooks/userApi.ts
@@ -6,15 +6,23 @@ const api = axios.create({
 
 export const useApi = () => ({
   validateToken: async (token: string) => {
+    return {
+      user: { id: 1, name: 'Usuário 01', email: 'usuario01@raj.com.br' },
+    }
     const response = await api.post('/validate', { token });
     return response.data;
   },
   signin: async (email: string, password: string) => {
+    return {
+      user: { id: 1, name: 'Usuário 01', email: 'usuario01@raj.com.br' },
+      token: '123456789',
+    }
     const response = await api.post(' /signin', { email, password });
     return response.data;
   },
   signout: async () => {
-    const response = await api.post('/logout');
+    return { status: true }
+    const response = await api.post('/signout');
     return response.data
   }
 });
