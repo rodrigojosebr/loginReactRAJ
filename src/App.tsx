e0d76e418257c1363390420f14f90441fb8516b2
Rodrigo José <rodrigo.angelis@grupoeureka.com.br>
2022-06-21 18:46:46 -0300

    logout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import { Link } from 'react-router-dom';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 import './App.css';
+import { AuthContext } from './contexts/Auth/AuthContext';
 import { Router } from './router';
 
 function App() {
+  const auth = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const handleSignout = () => {
+    auth.signout();
+    window.location.href = window.location.href;
+  }
+
   return (
     <div className="App">
       <header>
@@ -10,6 +20,7 @@ function App() {
         <nav>
           <Link to="/">Home</Link>
           <Link to="/private">Página Privada</Link>
+          {auth.user && <button onClick={handleSignout}>Sair</button>}
         </nav>
       </header>
       <hr />
